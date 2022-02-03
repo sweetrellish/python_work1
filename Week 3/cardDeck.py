5b825833dedbe19b1a3114210cd915e7c0b36c72
Ryan C. Ellis <98567257+sweetrellish@users.noreply.github.com>
2022-02-02 22:13:31 -0500

    Update cardDeck.py

--- Week 3/cardDeck.py
+++ Week 3/cardDeck.py
@@ -2,14 +2,17 @@ import hearts
 import diamonds
 import clubs
 import spades
+#import all card types
 
 plus = 0
 while plus < 13:
     cardDeck = [f"{hearts.heart_deck[plus]}", f"{diamonds.diamond_deck[plus]}", f"{clubs.club_deck[plus]}", f"{spades.spade_deck[plus]}"]
+#assign values of types to entired deck list
 count = 0 
 while count < 3:
     print (cardDeck[count])
     count += 1
+#print values of entired deck list
 
 
 
