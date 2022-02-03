8ea6fd3cd7772db55cf9dd5d273e2eea52d99c2f
Ryan C. Ellis <98567257+sweetrellish@users.noreply.github.com>
2022-02-03 09:50:30 -0500

    Testing import
    adding if block statement to cancel running code of imported card types

--- Week 3/cardDeck.py
+++ Week 3/cardDeck.py
@@ -4,15 +4,22 @@ import clubs
 import spades
 #import all card types
 
+cardDeck = []
 plus = 0
 while plus < 13:
-    cardDeck = [f"{hearts.heart_deck[plus]}", f"{diamonds.diamond_deck[plus]}", f"{clubs.club_deck[plus]}", f"{spades.spade_deck[plus]}"]
-#assign values of types to entired deck list
+    cardDeck.append(hearts.heart_deck[plus])
+    cardDeck.append(diamonds.diamond_deck[plus])
+    cardDeck.append(clubs.club_deck[plus])
+    cardDeck.append(spades.spade_deck[plus])
+    plus += 1
+#assign values of types to entire deck list
+
+print("\n\nHere's the entire deck:")
 count = 0 
-while count < 3:
+while count < 52:
     print (cardDeck[count])
     count += 1
-#print values of entired deck list
+#print values of entire deck
 
 
 
--- Week 3/hearts.py
+++ Week 3/hearts.py
@@ -1,41 +1,42 @@
-heart_deck = [] #total card number list
-count = 0 
-card = 2
-#initialize all variables
+if '__main__' == __name__:
+    heart_deck = [] #total card number list
+    count = 0 
+    card = 2
+    #initialize all variables
 
-while count < 9:
-    nameCard = f"{card} of Hearts"
-    heart_deck.append(nameCard)
-    card += 1
-    count += 1
-#while loop that adds card values to string list heart_deck
+    while count < 9:
+        nameCard = f"{card} of Hearts"
+        heart_deck.append(nameCard)
+        card += 1
+        count += 1
+    #while loop that adds card values to string list heart_deck
 
-faceCards = ['Ace of Hearts', 'Jack of Hearts', 'Queen of Hearts', 'King of Hearts']
-heart_deck.insert(0, faceCards[0])
-heart_deck.insert(count+1, faceCards[1])
-heart_deck.insert(count+2, faceCards[2])
-heart_deck.insert(count+3, faceCards[3])
+    faceCards = ['Ace of Hearts', 'Jack of Hearts', 'Queen of Hearts', 'King of Hearts']
+    heart_deck.insert(0, faceCards[0])
+    heart_deck.insert(count+1, faceCards[1])
+    heart_deck.insert(count+2, faceCards[2])
+    heart_deck.insert(count+3, faceCards[3])
 
-#print(heart_deck)
+    #print(heart_deck)
 
-print("Here's a list of all the Heart cards:")
-print_count = 0
-while print_count < 13:
-    print(heart_deck[print_count])
-    print_count += 1
-#use while loop to print all elements of heart deck
+    print("Here's a list of all the Heart cards:")
+    print_count = 0
+    while print_count < 13:
+        print(heart_deck[print_count])
+        print_count += 1
+    #use while loop to print all elements of heart deck
 
-print("\nHere's a list of number cards:")
-print2_count = 1
-while print2_count < 10:
-    print(heart_deck[print2_count])
-    print2_count += 1
-# while loop printing only number cards
+    print("\nHere's a list of number cards:")
+    print2_count = 1
+    while print2_count < 10:
+        print(heart_deck[print2_count])
+        print2_count += 1
+    # while loop printing only number cards
 
-print("\nHere's all the face cards:")
-print3_count = 1
-while print3_count < 4:
-    print(faceCards[print3_count])
-    print3_count += 1
-# while loop printing only face cards
- 
+    print("\nHere's all the face cards:")
+    print3_count = 1
+    while print3_count < 4:
+        print(faceCards[print3_count])
+        print3_count += 1
+    # while loop printing only face cards
+    
