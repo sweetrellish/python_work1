6548993df61f88878df43816e73a73db43835edc
Ryan C. Ellis <98567257+sweetrellish@users.noreply.github.com>
2022-02-24 18:03:25 -0500

    update

--- Week 5/club.py
+++ Week 5/club.py
@@ -1,9 +1,9 @@
-computer_club = ['andy', 'greg', 'michael j.', 'fox', 'admin']
+computer_club = ['andy', 'greg', 'michael j.', 'fox', 'ADMIN']
 #computer_club = ['']
 #comment code to switch to empty list
 
 for user in computer_club:
-    if user == 'admin':
+    if user.title() == 'Admin':
         print("Welcome Administrator!")
     elif user == '':
         print("Seek out more members!")
