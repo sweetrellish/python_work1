511ff9cd44d65dbe27e57d199e0748dd4c86bda6
Ryan C. Ellis <98567257+sweetrellish@users.noreply.github.com>
2022-10-08 18:33:12 -0400

    update

--- Problem Solving/test.py
+++ Problem Solving/test.py
@@ -1,5 +1,11 @@
-user1 = 'Toy'
-user2 = 'Constitution'
+digit1 = 0
+digit2 = 0
+digit3 = 0
 
-if user1 > user2 :
-    print("Hello!")
+for digit1 in range (10):
+    for digit2 in range (10):
+        for digit3 in range (10):
+            print(digit1, digit2, digit3)
+            digit3 += 1
+        digit2 += 1
+    digit1 += 1
